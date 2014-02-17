0e54088987c1f26bf009f02a5eee8c5f86747e4e
javieroropeza <j.a.och@hotmail.com>
2014-02-17 16:38:08 -0600

    segundo commit de opciones

--- encontrar la clave con random.cpp
+++ encontrar la clave con random.cpp
@@ -32,5 +32,98 @@ int buscar(int n[],int c,int t)
 		  return -1;
 
 	}
+	#include<stdio.h>
 
+int suma(int x, int y);
+int resta(int x, int y);
+int multiplicacion(int x, int y);
+float division(int x, int y);
+int potencia(int x, int y);
+
+
+	int op,num1,num2,sum,rest,mult,pot;
+	float div;
+	
+	printf("\n***** M  E  N  U *****\n");
+	printf("1. SUMA\n");
+	printf("2. RESTA\n");
+	printf("3. MULTIPLICACION\n");
+	printf("4. DIVISION\n");
+	printf("5. POTENCIA\n");
+	printf("6. SALIR\n");
+	scanf("%d",&op);
+	
+	printf("\nIngrese el primer valor\n");
+	scanf("%d",&num1);
+	printf("\nIngrese el segundo valor\n");
+	scanf("%d",&num2);
+	
+	switch(op)
+	{
+		case 1: {sum=suma(num1,num2);
+			printf("\nLa suma es: %d",sum);
+			};
+			break;
+		
+		case 2: {rest=resta(num1,num2);
+		printf("\nLa resta es: %d",rest);
+			};
+			break;
+			
+		case 3: {mult=multiplicacion(num1,num2);
+		printf("\nLa multiplicacion es: %d",mult);
+			};
+			break;
+		
+		case 4: {div=division(num1,num2);
+		printf("\nLa division es: %.2f",div);
+			};
+			break;
+		
+		case 5: {pot=potencia(num1,num2);
+		printf("\nLa potencia es: %d",pot);
+			};
+			break;
+		case 6: break;
+	}
+	
+	}
+
+int suma(int x, int y)
+{
+	return x+y;
 }
+
+
+int resta(int x, int y)
+{
+	return x-y;
+}
+
+
+int multiplicacion(int x, int y)
+{
+	return x*y;
+}
+
+
+float division(int x, int y)
+{
+	return x/y;
+}
+
+
+int potencia(int x, int y)
+{
+	int a;
+	int power;
+	power=1;
+	
+	for(a=1; a<=y; a++)
+	{
+		power = x*power;
+	};
+	
+	return power;
+}
+
